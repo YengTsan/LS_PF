39f546e964e49ec56f07190e670c7e4a759b3835
YengTsan <Yengttt@gmail.com>
2016-04-01 12:03:17 +0800

    Finished PF_Lesson2_Rubocop

---
+++ lesson_2/pseudo_code.rb
@@ -0,0 +1,37 @@
+=begin
+1. 
+
+get the first number
+get the second number
+
+add the two numbers together
+put the result to a veriable
+
+print the veriable
+
+
+2.
+
+set a blank string named result_string
+
+use the string in the array one by one ,from the first to the end , into the result_string
+
+print the result_string
+
+3.
+
+creat a blank array named result_array
+
+use the index to make the pair of the integer,
+
+if the index is even, create a array , and add the A[index] and the A[index+1] to a temp_array,
+and then  add the temp_array to result_array
+
+from the first to the end
+
+print the result_array
+
+=end
+
+
+

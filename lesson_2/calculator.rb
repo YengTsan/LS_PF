324a60ed9860492ff9463fde42c3b5ae5d5920eb
YengTsan <Yengttt@gmail.com>
2016-04-01 17:08:10 +0800

    Finished the Lesson_Rubocop_Calculator

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,29 +1,103 @@
-
-
-Kernel.puts("Welcome to Calculator")
-
-Kernel.puts("What's the first number1?")
-number1 = Kernel.gets().chomp().to_i
-
-Kernel.puts("")
-
-Kernel.puts("What's the second number2?")
-number2 = Kernel.gets().chomp().to_i
-
-Kernel.puts("What's operation would you like to perform? 1) add 2)subtrace 3)mutiply 4)divide")
-operator = Kernel.gets().chomp()
-
-case
-when operator == "1"
-  result = number1 + number2
-when operator == "2"
-  result = number1 - number2
-when operator == "3"
-  result = number1 * number2
-when operator == "4"
-  result = number1.to_f / number2.to_f
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
 
+def valid_number?(num)
+  num != 0
+end
 
-Kernel.puts("The result is #{result}")
+def operation_to_message(op)
+  case op
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracing'
+  when '3'
+    'Mutiplying'
+  when '4'
+    'Dividing'
+  end
+end
 
+number1 = nil
+number2 = nil
+name = nil
+operator = nil
+
+prompt("Welcome to Calculator Enter your name:")
+
+loop do
+  name = Kernel.gets().chomp()
+
+  if name.empty?()
+    prompt("Make sure to use a valid name.")
+  else
+    break
+  end
+end
+
+prompt("Hi, #{name}")
+
+loop do # main loop
+  loop do
+    prompt("What's the first number1?")
+    number1 = Kernel.gets().chomp().to_i()
+
+    if valid_number?(number1)
+      break
+    else
+      prompt("Hmm....thst doesn't look like a valid number")
+    end
+  end
+
+  loop do
+    prompt("What's the second number2?")
+    number2 = Kernel.gets().chomp().to_i()
+
+    if valid_number?(number2)
+      break
+    else
+      prompt("Hmm....thst doesn't look like a valid number")
+    end
+  end
+
+  operator_prompt = <<-MSG
+    What operation would you like to perform
+    1)add
+    2)subtrace
+    3)mutiply
+    4)divide
+  MSG
+
+  prompt(operator_prompt)
+
+  loop do
+    operator = Kernel.gets().chomp()
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Must choose 1, 2, 3or 4")
+    end
+  end
+
+  prompt("#{operation_to_message(operator)} the two number..")
+
+  result = case operator
+           when "1"
+             number1 + number2
+           when "2"
+             number1 - number2
+           when "3"
+             number1 * number2
+           when "4"
+             number1.to_f / number2.to_f
+           end
+
+  prompt("The result is #{result}")
+
+  prompt("Do you want to perform another calculation? (Y to calculator agiain)")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt ("Thank you for using the calculator. Good bye!")
---
+++ lesson_2/calculator_origin.rb
@@ -0,0 +1,27 @@
+
+
+Kernel.puts("Welcome to Calculator")
+
+Kernel.puts("What's the first number1?")
+number1 = Kernel.gets().chomp().to_i
+
+Kernel.puts("What's the second number2?")
+number2 = Kernel.gets().chomp().to_i
+
+Kernel.puts("What's operation would you like to perform? 1) add 2)subtrace 3)mutiply 4)divide")
+operator = Kernel.gets().chomp()
+
+case
+when operator == "1"
+  result = number1 + number2
+when operator == "2"
+  result = number1 - number2
+when operator == "3"
+  result = number1 * number2
+when operator == "4"
+  result = number1.to_f / number2.to_f
+end
+
+
+Kernel.puts("The result is #{result}")
+
